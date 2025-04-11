5ca9e525fab1490054208b2e24efd0c224d00c59
sidvashisth2005 <siddhantvashisth05@gmail.com>
2025-04-11 16:15:13 +0530

    Login page has been made!! It's April 10, 2025 (11:45 PM)

--- app/src/main/java/com/example/homerentalsapp/screens/LoginActivity.java
+++ app/src/main/java/com/example/homerentalsapp/screens/LoginActivity.java
@@ -18,6 +18,7 @@ public class LoginActivity extends AppCompatActivity {
     private TextView createAccount;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
