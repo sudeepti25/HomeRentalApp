8badd71ef3a809d13c8f0f64c34e9930ef9a916a
sidvashisth2005 <siddhantvashisth05@gmail.com>
2025-04-11 10:06:06 +0530

    Login page has been made!! It's April 10, 2025 (11:45 PM)

--- app/src/main/java/com/example/homerentalsapp/screens/LoginActivity.java
+++ app/src/main/java/com/example/homerentalsapp/screens/LoginActivity.java
@@ -18,7 +18,7 @@ public class LoginActivity extends AppCompatActivity {
     private TextView createAccount;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
@@ -27,7 +27,7 @@ public class LoginActivity extends AppCompatActivity {
             createAccount.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    startActivity(new Intent(LoginActivity.this, SignUpActivity.class) );
+                    startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
                 }
             });
     }
